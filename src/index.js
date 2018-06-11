7ff3fc702a04a2d929acc01bac61a369f9bb1ab5
Kolia Nalbandian <kolia.nalbandian@gmail.com>
2018-06-11 22:31:14 +0300

    clean up code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import Chart from './modules/chart';
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
-
-// Require exports file with endpoint and auth info
-const aws_exports = require('./aws-exports').default;
+import AWS from 'aws-sdk';
+// Import gql helper and craft a GraphQL query
+import gql from 'graphql-tag';
 
 // Require AppSync module
-const AUTH_TYPE = require('aws-appsync/lib/link/auth-link').AUTH_TYPE;
-const AWSAppSyncClient = require('aws-appsync').default;
+import {AUTH_TYPE} from 'aws-appsync/lib/link/auth-link';
+import AWSAppSyncClient from 'aws-appsync';
+
+// Require exports file with endpoint and auth info
+import aws_exports from './aws-exports';
+
+require('es6-promise').polyfill();
+import 'isomorphic-fetch';
 
 const url = aws_exports.ENDPOINT;
 const region = aws_exports.REGION;
@@ -15,23 +19,12 @@ const type = AUTH_TYPE.API_KEY;
 
 // If you want to use API key-based auth
 const apiKey = 'da2-vrivdjhd4vd2tng7euea3ji5eq';
-// If you want to use a jwtToken from Amazon Cognito identity:
-const jwtToken = 'xxxxxxxx';
 
-// If you want to use AWS...
-const AWS = require('aws-sdk');
 AWS.config.update({
-  region: aws_exports.REGION,
-  // credentials: new AWS.Credentials({
-  //     accessKeyId: aws_exports.AWS_ACCESS_KEY_ID,
-  //     secretAccessKey: aws_exports.AWS_SECRET_ACCESS_KEY
-  // })
+  region: aws_exports.REGION
 });
-// const credentials = AWS.config.credentials;
 
-// Import gql helper and craft a GraphQL query
-const gql = require('graphql-tag');
-const query = gql(`
+const listItemsQuery = gql(`
 query listItems {
   listItems {
     items {
@@ -44,7 +37,7 @@ query listItems {
 }`);
 
 // Set up a subscription query
-const subquery = gql(`
+const onCreateItemsQuery = gql(`
 subscription onCreateItems {
 onCreateItems {
     __typename
@@ -61,8 +54,7 @@ const client = new AWSAppSyncClient({
   region: region,
   auth: {
     type: type,
-    apiKey: apiKey,
-    // credentials: credentials,
+    apiKey: apiKey
   },
   disableOffline: true
 });
@@ -71,7 +63,7 @@ const chart = new Chart();
 
 client.hydrated().then(function (client) {
   //Now run a query
-  client.query({query: query})
+  client.query({query: listItemsQuery})
     .then(function logData(data) {
       console.log('results of query: ', data);
       chart.updateChart(data.data.listItems.items);
@@ -79,7 +71,7 @@ client.hydrated().then(function (client) {
     .catch(console.error);
 
   //Now subscribe to results
-  const observable = client.subscribe({query: subquery});
+  const observable = client.subscribe({query: onCreateItemsQuery});
 
   const realtimeResults = function realtimeResults(data) {
     console.log('realtime data: ', data);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const webpack = require('webpack');
 const path = require('path');
 
+const NODE_ENV = process.env.NODE_ENV.trim();
+const isDevelopment = NODE_ENV === 'development';
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  watch: process.env.WEBPACK_MODE !== 'production',
+  watch: isDevelopment,
   watchOptions: {
     aggregateTimeout: 300,
     poll: 1000
@@ -32,7 +35,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
     })
   ],
   node: {
